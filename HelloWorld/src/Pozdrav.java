e0413bf857d43e3d836a1142af3b5dd26e106ccb
Pedja <r_predrag@yahoo.com>
2013-09-29 15:12:01 +0200

    another change

--- HelloWorld/src/Pozdrav.java
+++ HelloWorld/src/Pozdrav.java
@@ -4,6 +4,7 @@ public class Pozdrav {
 		System.out.println("Hello");
 		System.out.println("Push to remote repository");
 		System.out.println("This is created in the new branch 'devel'");
+		System.out.println("Why the change is not seen?");
 	}
 
 }
