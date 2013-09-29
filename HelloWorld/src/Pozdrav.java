bbc11f8c716c3132be98ae2f1ea71cd4c10c602d
Pedja <r_predrag@yahoo.com>
2013-09-29 14:54:21 +0200

    devel branch

--- HelloWorld/src/Pozdrav.java
+++ HelloWorld/src/Pozdrav.java
@@ -3,6 +3,7 @@ public class Pozdrav {
 	public static void main(String [] args){
 		System.out.println("Hello");
 		System.out.println("Push to remote repository");
+		System.out.println("This is created in the new branch 'devel'");
 	}
 
 }
