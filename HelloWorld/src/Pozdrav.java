ab3fd2ba06d6d930d3496641fe8ddb05c8c09150
pedja <pedja@pedja-PC>
2014-06-15 16:33:37 +0200

    "testPozdrav" introduced

--- HelloWorld/src/Pozdrav.java
+++ HelloWorld/src/Pozdrav.java
@@ -1,11 +1,7 @@
 
 public class Pozdrav {
-	public static void main(String [] args){
-		//System.out.println("Hello");
-		System.out.println("Push to remote repository");
-		System.out.println("This is created in the new branch 'devel'");
-		System.out.println("Why the change is not seen?");
-		System.out.println("Testing hard reset");
+	void testPozdrav(){
+		
 	}
 
 }
